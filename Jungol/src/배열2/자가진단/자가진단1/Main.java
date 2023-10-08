101796acb7894893fdfd44644c7d4863ef6545fb
JiHye0214 <qkrwlgp1526@gmail.com>
2023-10-08 23:51:19 +0900

    231008 : add Jungol practice

--- Jungol/src/배열2/자가진단/자가진단1/Main.java
+++ Jungol/src/배열2/자가진단/자가진단1/Main.java
@@ -1,4 +1,17 @@
 package 배열2.자가진단.자가진단1;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] array = new char[100];
+        char alpa, num;
+        int i, j;
+
+
+
+        sc.close();
+    }
 }
