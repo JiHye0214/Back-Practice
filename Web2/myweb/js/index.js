4567030b4f94fa8f428e813d9fa8d34cd7a47590
JiHye0214 <qkrwlgp1526@gmail.com>
2023-10-30 22:07:20 +0900

    231030 : git commit backend task

---
+++ Web2/myweb/js/index.js
@@ -0,0 +1,4 @@
+function goToKiaPage() {
+    alert("기아 타이거즈 V12")
+    location.href = 'http://127.0.0.1:5501/Web2/myweb/main.html'
+}
--- Web2/myweb/js/login.js
+++ Web2/myweb/js/login.js
@@ -1,4 +1,4 @@
-function goToKiaPage() {
-    alert("기아타이거즈")
+function goToLogIn(){
+    alert("로그인 먼저 하세요");
     location.href = 'http://127.0.0.1:5501/Web2/myweb/signForm.html'
 }
