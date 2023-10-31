4c80433907b443861bd475bcfb4080709dd64104
JiHye0214 <qkrwlgp1526@gmail.com>
2023-10-31 15:10:13 +0900

    231031 : git commit backend task

--- Web2/myweb/js/login.js
+++ Web2/myweb/js/login.js
@@ -2,3 +2,20 @@ function goToLogIn(){
     alert("로그인 먼저 하세요");
     location.href = 'http://127.0.0.1:5501/Web2/myweb/signForm.html'
 }
+
+function BTNlogin() {
+    const frm = document.forms['form'];
+    
+    if(frm['id'].value.trim() == "" || frm['pw'].value.trim() == "" ){
+        showId();
+        return false;
+    }
+    return true;
+}
+
+function showId(){
+    const $msg = $('#warn-message');
+    const $wrapper = $('#login-group');
+    $wrapper.animate({'height' : '355px'}, 0)
+    $msg.show()
+}
