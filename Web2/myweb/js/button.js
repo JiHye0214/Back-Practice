f18af8c513eeb614a532a6277c7713c5751b92bb
JiHye0214 <qkrwlgp1526@gmail.com>
2023-11-01 22:30:24 +0900

    231101 : git commit backend task

--- Web2/myweb/js/button.js
+++
@@ -1,4 +0,0 @@
-function goToKiaPage() {
-    alert("기아타이거즈")
-    location.href = 'http://127.0.0.1:5501/Web2/myweb/signForm.html'
-}
--- Web2/myweb/js/login.js
+++ Web2/myweb/js/login.js
@@ -1,21 +1,29 @@
-function goToLogIn(){
-    alert("로그인 먼저 하세요");
-    location.href = 'http://127.0.0.1:5501/Web2/myweb/signForm.html'
+function goToLogIn() {
+  alert("로그인 먼저 하세요");
+  location.href = "http://127.0.0.1:5501/Web2/myweb/signForm.html";
 }
 
 function BTNlogin() {
-    const frm = document.forms['form'];
-    
-    if(frm['id'].value.trim() == "" || frm['pw'].value.trim() == "" ){
-        showId();
-        return false;
-    }
-    return true;
-}
+  const frm = document.forms["form"];
 
-function showId(){
-    const $msg = $('#warn-message');
-    const $wrapper = $('#login-group');
-    $wrapper.animate({'height' : '355px'}, 0)
-    $msg.show()
+  const $id = $("#warn-id");
+  const $pw = $("#warn-pw");
+  const $wrapper = $("#login-group");
+
+  // form validation
+  if (frm["id"].value.trim() == "") {
+      $pw.hide();
+      $wrapper.animate({ height: "355px" }, 0);
+      $id.show();
+    return false;
+  } else if (frm["pw"].value.trim() == "") {
+      $id.hide();
+      $wrapper.animate({ height: "355px" }, 0);
+      $pw.show();
+    return false;
+  }
+  
+  alert(" 환영합니다");
+  // 대체 왜 이동을 안 하는 거지
+  return true;
 }
